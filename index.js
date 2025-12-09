95d7d07cc7d819d138fffb00cf279f4c13909936
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-09 23:02:00 +0500

    update latest Code

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ var Name = "Ahsan Ali";
 console.log("Name", Name);
 console.log("Age", Age);
 //Browser ka Window object mai already name Naam ka 1 variable hota hay isliye hum var Name likkhtay hein 
+var NumberAll = [15, 23, 223, 53434, 231];
+var max = NumberAll[0];
+var min = NumberAll[0];
+for (var i = 0; i < NumberAll.length; i++) {
+    if (NumberAll[i] > max)
+        max = NumberAll[i];
+    if (NumberAll[i] < min)
+        min = NumberAll[i];
+}
+console.log("Max:", max);
+console.log("Min:", min);
--- index.ts
+++ index.ts
@@ -5,3 +5,15 @@ console.log("Name",Name);
 console.log("Age",Age);
 
 //Browser ka Window object mai already name Naam ka 1 variable hota hay isliye hum var Name likkhtay hein 
+
+const NumberAll: number[] =[15,23,223,53434,231];
+let max: number = NumberAll[0];  
+let min: number = NumberAll[0];  
+
+for(let i=0;i<NumberAll.length;i++){
+    if(NumberAll[i]>max) max = NumberAll[i]
+    if(NumberAll[i]<min) min = NumberAll[i]
+}
+
+console.log("Max:", max);
+console.log("Min:", min);
