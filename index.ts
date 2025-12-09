0bf6f97f20cff734e79c85136273d6bc22c67813
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-10 02:41:25 +0500

    update code

--- index.ts
+++ index.ts
@@ -7,8 +7,8 @@ console.log("Age",Age);
 //Browser ka Window object mai already name Naam ka 1 variable hota hay isliye hum var Name likkhtay hein 
 
 const NumberAll: number[] =[15,23,223,53434,231];
-let max: number = NumberAll[0];  
-let min: number = NumberAll[0];  
+let max:number = NumberAll[0];  
+let min:number = NumberAll[0];  
 
 for(let i=0;i<NumberAll.length;i++){
     if(NumberAll[i]>max) max = NumberAll[i]
---
+++ primitive.ts
@@ -0,0 +1,5 @@
+const number1:number=10;
+const number2:number=20;
+const number3:number=30;
+
+console.log(number1+number2+number3);
