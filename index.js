abed18f57c249cc96651b55b5acd67e4ec989a20
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-09 22:36:06 +0500

    Code push

---
+++ index.js
@@ -0,0 +1,5 @@
+var Age = 20;
+var Name = "Ahsan Ali";
+console.log("Name", Name);
+console.log("Age", Age);
+//Browser ka Window object mai already name Naam ka 1 variable hota hay isliye hum var Name likkhtay hein 
---
+++ index.ts
@@ -0,0 +1,7 @@
+var Age:number=20;
+var Name:string="Ahsan Ali";
+
+console.log("Name",Name);
+console.log("Age",Age);
+
+//Browser ka Window object mai already name Naam ka 1 variable hota hay isliye hum var Name likkhtay hein 
