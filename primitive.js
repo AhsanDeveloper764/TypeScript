75bd7fbfd64ef75cf3c83f04306b3b841868524d
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-12 02:29:21 +0500

    update

--- primitive.js
+++ primitive.js
@@ -1,4 +1,22 @@
+// Number Data type
 var number1 = 10;
 var number2 = 20;
 var number3 = 30;
-console.log(number1 + number2 + number3);
+var total = number1 + number2 + number3;
+console.log("Total", total);
+var octaDecimal = 32769;
+var hex = 1;
+var binary = 0x00001;
+console.log(octaDecimal + hex + binary + 31);
+var item = 100;
+var item2 = "50";
+var item2Converted = Number(item2);
+var sum = item2Converted + item;
+// const item2Converted=+item2 
+console.log("sum", sum);
+// String And Boolean Data Type
+var str = "Ahsan Ali";
+var age = 20;
+var data = age.toString();
+var info = "my name is ".concat(str, " and my age is ").concat(age);
+console.log(info);
--- primitive.ts
+++ primitive.ts
@@ -1,5 +1,28 @@
+// Number Data type
 const number1:number=10;
 const number2:number=20;
 const number3:number=30;
 
-console.log(number1+number2+number3);
+const total:number=number1+number2+number3
+console.log("Total",total);
+
+const octaDecimal:number = 0o100001;
+const hex:number = 0b00001;
+const binary:number = 0x00001;
+
+console.log(octaDecimal+hex+binary+31);
+
+const item:number=100;
+const item2:string="50"
+
+const item2Converted = Number(item2)
+const sum=item2Converted+item;
+// const item2Converted=+item2 
+console.log("sum",sum);
+
+// String And Boolean Data Type
+const str:string = "Ahsan Ali"
+const age:number = 20
+const data = age.toString();
+let info:string = `my name is ${str} and my age is ${age}`
+console.log(info);
